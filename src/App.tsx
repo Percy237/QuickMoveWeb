31b54e8758c7eb8c6994fbed64e1ab2eecfc6a77
Percy237 <percylarson17@gmail.com>
2024-10-15 16:27:43 +0100

    build for production

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,4 @@
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./layout/Layout";
 import LandingPage from "./pages/LandingPage";
 
